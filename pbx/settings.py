f6078e9596c49d9e12ef225ed9cb0b88a65865a2
AdrianFretwell <adrian@djangopbx.com>
2022-05-06 07:29:17 +0100

    Updated secret key, thi swill be changed by the install script

--- pbx/settings.py
+++ pbx/settings.py
@@ -49,7 +49,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x^2%vkujz31fgtsu7@*7lqskkw9vq9k4n(n!@zig$ku4!wc(4p'
+SECRET_KEY = 'django-insecure-aaabbbcccdddeeefff9876543210'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
