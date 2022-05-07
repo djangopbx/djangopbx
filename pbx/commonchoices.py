a65d5b8d78c81d605381ef181130c44dc3959c39
AdrianFretwell <adrian.fretwell@topgreen.co.uk>
2022-05-07 15:43:38 +0100

    Added Portal app - provides menu system etc.

---
+++ pbx/commonchoices.py
@@ -0,0 +1,88 @@
+#
+#    DjangoPBX
+#
+#    MIT License
+#
+#    Copyright (c) 2016 - 2022 Adrian Fretwell <adrian@djangopbx.com>
+#
+#    Permission is hereby granted, free of charge, to any person obtaining a copy
+#    of this software and associated documentation files (the "Software"), to deal
+#    in the Software without restriction, including without limitation the rights
+#    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#    copies of the Software, and to permit persons to whom the Software is
+#    furnished to do so, subject to the following conditions:
+#
+#    The above copyright notice and this permission notice shall be included in all
+#    copies or substantial portions of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#    SOFTWARE.
+#
+#    Contributor(s):
+#    Adrian Fretwell <adrian@djangopbx.com>
+#
+
+#
+#  These are Choices classes used by more than one application
+#
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class EnabledTrueFalseChoice(models.TextChoices):
+    CTRUE  = 'true',  _('True')
+    CFALSE = 'false', _('False')
+
+
+class PrimaryTrueFalseChoice(models.IntegerChoices):
+    CTRUE  = 1, _('True')
+    CFALSE = 0, _('False')
+
+
+class EnabledDisabledChoice(models.TextChoices):
+    CENABLED  = 'true',  _('Enabled')
+    CDISABLED = 'false', _('Disabled')
+
+
+class ConfirmChoice(models.TextChoices):
+    CNONE    = '',  _('')
+    CCONFIRM = '1', _('Confirm')
+
+
+class SettingTypeChoice(models.TextChoices):
+    CTEXT    = 'text'   , _('text')
+    CNUMERIC = 'numeric', _('numeric')
+    CARRAY   = 'array'  , _('array')
+    CBOOLEAN = 'boolean', _('boolean')
+    CCODE    = 'code'   , _('code')
+    CUUID    = 'uuid'   , _('uuid')
+    CNAME    = 'name'   , _('name')
+    CVAR     = 'var'    , _('var')
+    CDIR     = 'dir'    , _('dir')
+
+
+class StatusDefaultChoice(models.TextChoices):
+    CNONE         = ''                      , _('Not Set')
+    CAVAILABLE    = 'Available'             , _('Available')
+    CAVAILABLEOD  = 'Available (On Demand)' , _('Available (On Demand)')
+    CLOGGEDOUT    = 'Logged Out'            , _('Logged Out')
+    CONBREAK      = 'On Break'              , _('On Break')
+    CDONOTDISTURB = 'Do Not Disturb'        , _('Do Not Disturb')
+
+
+class TargetCategoryChoice(models.TextChoices):
+    CINTERNAL = 'internal' , _('Internal')
+    CEXTERNAL = 'external' , _('External')
+    CEMAIL    = 'e-mail'   , _('E-Mail')
+
+
+class CallDirectionChoice(models.TextChoices):
+    CINBOUND  = 'inbound' , _('Inbound')
+    COUTBOUND = 'outbound', _('Outbound')
+
--- portal/urls.py
+++ portal/urls.py
@@ -27,26 +27,16 @@
 #    Adrian Fretwell <adrian@djangopbx.com>
 #
 
-"""pbx URL Configuration
+from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import include, path
+from . import views
 
 urlpatterns = [
-    path(''       , include('portal.urls')),
-    path('portal/', include('portal.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
